3c88810b3f703235403bc432c8d4d4ae6194c705
nnaelle <annaelle.duff@gmail.com>
2016-10-01 14:12:29 -0400

    Adding support for python3

--- django_xhtml2pdf/utils.py
+++ django_xhtml2pdf/utils.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 from django.template.context import Context
 from django.template.loader import get_template
 from xhtml2pdf import pisa # TODO: Change this when the lib changes.
-import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 import os
 
 #===============================================================================
@@ -62,7 +65,7 @@ def generate_pdf(template_name, file_object=None, context=None, link_callback=fe
 
     """
     if not file_object:
-        file_object = StringIO.StringIO()
+        file_object = StringIO()
     if not context:
         context = {}
     tmpl = get_template(template_name)
